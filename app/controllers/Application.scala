cd9b4bc89f202a5f1844cb9e4e471741e0d1f7ec
Mattia Amato <mattiamato@gmail.com>
2014-11-20 13:10:03 +0100

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Redirect(routes.Application.viewer)
+  }
+
+  def viewer = TODO
+
+}
