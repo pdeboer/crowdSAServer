c73ebf54b0950f0dc78cc6557916afe11048c4d6
Mattia Amato <mattia.amato@uzh.ch>
2015-05-15 14:30:59 +0200

    Fix minor bug to display missing question

--- app/controllers/Viewer.scala
+++ app/controllers/Viewer.scala
@@ -62,10 +62,10 @@ object Viewer extends Controller{
 
               var jumpTo = ""
               if(highlights.length>0 && question.question_type == "Discovery"){
-                jumpTo = dataset_highlight.head
-                //jumpTo = question.question.substring(question.question.indexOf("<i> ") + 4, question.question.indexOf("</i>")-1)
-              }else if(highlights.length>0 && question.question_type != "Discovery") {
                 jumpTo = highlights.head
+                //jumpTo = question.question.substring(question.question.indexOf("<i> ") + 4, question.question.indexOf("</i>")-1)
+              }else if(highlights.length>0 && question.question_type != "Discovery" && question.question_type != "Missing") {
+                jumpTo = dataset_highlight.head
               } else {
                 jumpTo = ""
               }
